e17e8fa240dee87fb60be6e2765f77ca3df4e676
K1NGAARON <gerardaaron1997@gmail.com>
2023-12-23 20:34:37 +0100

    blog post template

---
+++ blog/js/relatec-content.js
@@ -0,0 +1,62 @@
+const content = [
+    {
+        title: "Hiele Defreyne",
+        tag: "website-development",
+        body: "",
+        img: "/klanten/hiele-defreyne/img/banner.jpg",
+        url: "/klanten/hiele-defreyne/"
+    },
+    {
+        title: "Coin Perdu",
+        tag: "advertising",
+        body: "",
+        img: "/klanten/coin-perdu/img/banner.jpg",
+        url: "/klanten/coin-perdu"
+    },
+    {
+        title: "Vloeren Heyrick",
+        tag: "website-development",
+        body: "",
+        img: "/klanten/vloeren-heyrick/img/banner.jpg",
+        url: "/klanten/vloeren-heyrick/"
+    },
+    // {
+    //     title: "",
+    //     tag: "",
+    //     body: "",
+    //     img: "/portfolio/",
+    //     url: "/portfolio/"
+    // },
+];
+
+function createCards(e) {
+    const target = document.querySelector('#content');
+    const shuffledArray = content.sort((a, b) => 0.5 - Math.random());
+    
+    const cards = shuffledArray.map(item => {
+
+        return `
+            <div class="item card ${item.tag}">
+                <img src="${item.img}" alt="${item.title}">
+                <div class="content">
+                    <h3>
+                        ${item.title}
+                    </h3>
+                    <p>
+                        ${item.body}
+                    </p>
+                    <a href="${item.url}" class="btn">
+                        Lees meer
+                        <i class="fa-solid fa-angle-right"></i>
+                    </a>
+                </div>
+            </div>
+        `;
+    }).join('');
+
+    target.innerHTML = cards;
+}
+
+$(document).ready(function() {
+    createCards();
+});
